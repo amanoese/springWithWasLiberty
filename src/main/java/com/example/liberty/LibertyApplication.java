dec69a7ccc82c75a4c30b37ea0e40f114d57e65a
Seito Taka <amanoese@gmail.com>
2018-04-19 18:36:43 +0900

    first commit

---
+++ src/main/java/com/example/liberty/LibertyApplication.java
@@ -0,0 +1,23 @@
+package com.example.liberty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@SpringBootApplication
+public class LibertyApplication extends SpringBootServletInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibertyApplication.class, args);
+	}
+	
+	@RequestMapping("/")
+	@ResponseBody
+	public String index() {
+		return "Hello,World!";
+	}
+}
---
+++ src/test/java/com/example/liberty/LibertyApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.liberty;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LibertyApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
